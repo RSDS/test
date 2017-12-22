a9eadaeb6c9c2f83dcd0705d0c26f1dce27be3ab
徐少斌 <xushaobin@quyiyuan.com>
2017-12-22 21:06:11 +0800

    完成finviz基本面数据抓取

--- tutorial/tutorial/spiders/finviz.py
+++ tutorial/tutorial/spiders/finviz.py
@@ -3,8 +3,11 @@ import scrapy
 from scrapy_splash import SplashRequest
 
 
-class WBSpider(scrapy.Spider):
+# 市值大于2B 的公司数据
+class StockSpider(scrapy.Spider):
     name = "finviz"
+    origin_url = 'https://finviz.com/'
+
     start_urls = [
         'https://finviz.com/screener.ashx?v=111&f=cap_midover'
     ]
@@ -14,12 +17,24 @@ class WBSpider(scrapy.Spider):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
     }
 
+    #
+    # def generate_urls(self):
+    #     start_url = self.origin_url+'screener.ashx?v=111&f=cap_midover&r='
+    #     for i in range(1,89):
+    #         start_url += i*20+1
+    #         self.start_urls.append(start_url)
+
 
 
 
 
     def start_requests(self):
-        yield SplashRequest(url=self.start_urls[0], headers=self.headers,  args={'wait': 3})
+        for i in range(1,89):
+            start_url = self.origin_url+'screener.ashx?v=111&f=cap_midover&r='
+            start_url += str(i*20+1)
+            self.start_urls.append(start_url)
+        for url in self.start_urls:
+            yield SplashRequest(url=url, headers=self.headers,  args={'wait': 10})
 
 
 
@@ -34,22 +49,38 @@ class WBSpider(scrapy.Spider):
         print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++爬虫开始+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         companyList = response.xpath('//*[@id="screener-content"]/table/tbody/tr[4]/td/table/tbody/tr[position()>1]')
 
+        sql = 'insert into t_company_fundamental(ticker, company_name, sector, industry, country, market_cap, price_earnings_ratio, price, price_change, volume) VALUES'
         list2 = []
         for company in companyList:
             fundamentals = company.xpath('./td[position()>1]')
             list1 = []
             for fundamental in fundamentals:
                 item = fundamental.xpath('./a/text()').extract_first()
+                if(item==None):
+                    item = fundamental.xpath('./a/span/text()').extract_first()
                 list1.append(item)
+
+            sql+='(\"'+list1[0]+'\", \"'+list1[1]+'\", \"'+list1[2]+'\", \"'+list1[3]+'\", \"'+list1[4]+'\", \"'+list1[5]+'\", \"'+list1[6]+'\", \"'+list1[7]+'\", \"'+list1[8]+'\", \"'+list1[9]+'\"), '
 
             list2.append(list1)
 
-            # cursor.execute('insert into t_text(name, content) VALUES (%s, %s)', ['梁斌penny', y.strip()])
+        print(sql[0:sql.__len__()-2])
+        cursor.execute(sql[0:sql.__len__()-2])
 
-        print(list2)
+
 
         conn.commit()
         cursor.close()
 
         print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++爬虫结束+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
+
+
+
+
+
+start_url = 'screener.ashx?v=111&f=cap_midover&r='
+for i in range(1,89):
+    start_url = 'screener.ashx?v=111&f=cap_midover&r='
+    start_url += str(i*20+1)
+    print(start_url)
--- tutorial/tutorial/spiders/finviz.sql
+++ tutorial/tutorial/spiders/finviz.sql
@@ -1,13 +1,32 @@
 -- 公司基本信息表
 DROP TABLE IF EXISTS t_company_fundamental;
 CREATE TABLE t_company_fundamental(
-    company_fundamental_key_id int(11) NOT NULL AUTO_INCREMENT,
-    ticker varchar(50),
+    company_fundamental_key_id int(30) NOT NULL AUTO_INCREMENT,
+    ticker varchar(50)                                    comment  '老式股票收报机，目前指股票标的',
     company_name VARCHAR(200),
-    sector VARCHAR(100) comment'所属行业大类',
-    industry VARCHAR(200) comment'所属行业子类',
+    sector VARCHAR(100)                                   comment  '所属行业大类',
+    industry VARCHAR(200)                                 comment  '所属行业子类',
     country VARCHAR(100),
-    market_cap VARCHAR(100) comment'市值',
+    market_cap VARCHAR(100)                               comment  '市值',
+    price_earnings_ratio VARCHAR(100) comment'市盈率',
+    price varchar(100),
+    price_change varchar(100),
+    volume varchar(100),
+    primary key (company_fundamental_key_id)
+)ENGINE=InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET= utf8;
 
 
-)
+
+-- 分析师行动
+DROP TABLE IF EXISTS t_analyst_action;
+CREATE TABLE t_analyst_action(
+    analyst_action_key_id int(30) NOT NULL AUTO_INCREMENT,
+    ticker varchar(50),
+    investment_bank varchar(100) comment '投行',
+    action_date varchar(50)      comment '行动日期',
+    action_overview varchar(100)  comment '行动概览',
+    action_grade_detail varchar(100)    comment '行动级明细',
+    action_price_detail varchar(100)    comment '行动价格明细',
+    primary key (analyst_action_key_id)
+
+)ENGINE=InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET= utf8;
